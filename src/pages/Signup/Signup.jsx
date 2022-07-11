6205c67326a4bab3f748e258e7a34305b3488d09
alexis00rod <alexis00rodrigo@gmail.com>
2022-07-11 05:06:08 -0300

    Corregui error en la funcion submit

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,23 +1,24 @@
+import { signupAuth } from '../../services/getData'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-import { signup } from '../../services/getData'
+
 
 export const Signup = () => {
     const [userToSignup, setUserToSignup] = useState({})
-    const [signUp, setSignUp] = useState()
+    const [signup, setSignup] = useState()
     const navigate = useNavigate()
 
-    const signUpUser = async (e) => {
+    const signupUser = async (e) => {
         e.preventDefault()
         try {
-            await signup(userToSignup.userEmail,userToSignup.userPass)
+            await signupAuth(userToSignup.email,userToSignup.password)
             navigate("/login")
         } catch (err) {
-            setSignUp(`${err.code}`)
+            setSignup(err.code)
         }
     }
 
-    const handleUser = ({target: {name,value}}) => {
+    const handleUserToSignup = ({target: {name,value}}) => {
         setUserToSignup({
             ...userToSignup,
             [name]: value
@@ -29,15 +30,12 @@ export const Signup = () => {
             <h1>Signup</h1>
             <section>
                 {
-                    // register 
-                    // ? <p>Resgister!</p>
-                    // : <p></p>
-                    signUp && <p>{signUp}</p>   
+                    signup && <p>{signup}</p> 
                 }
-                <form className="flex flex-col ali-center gap-m" onSubmit={signUpUser}>
-                    <input type="email" name="userEmail" id="userEmail" placeholder="Enter your email" className="input" onChange={handleUser}/>
-                    <input type="password" name="userPass" id="userPass" placeholder="Enter your password" className="input"onChange={handleUser}/>
-                    <button className="btn btn-pri"><span>Login</span></button>
+                <form className="flex flex-col ali-center gap-m" onSubmit={signupUser}>
+                    <input type="email" name="email" placeholder='Enter your email' className='input' onChange={handleUserToSignup} />
+                    <input type="password" name="password" placeholder='Enter your password' className='input' onChange={handleUserToSignup} />
+                    <button className="btn btn-pri"><span>Signup</span></button>
                 </form>
                 <div>
                     <p>Do you already have an account created?<Link to="/login">Log in</Link></p>
