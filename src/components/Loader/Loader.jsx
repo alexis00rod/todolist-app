3aa00c953518361875b44e826a473add3c9e6cfd
alexis00rod <alexis00rodrigo@gmail.com>
2022-07-13 02:35:37 -0300

    Agregue componente loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+export const Loader = () => {
+    return (
+        <div className="loader">
+            <span>Loading</span>
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+        </div>
+    )
+}
